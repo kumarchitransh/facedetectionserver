a5e64b4186e537689e7bbc9c65dc8cbbc35b23ec
Kumar Chitransh <kumar.chitransh-coep@bvucoep.edu.in>
2021-05-11 22:12:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,35 +4,44 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+//Controllers
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+// database connection
 const db = knex({
   client: 'pg',
   connection: {
-    //connectionString : process.env.DATABASE_URL,
-    //ssl: true 
-    host : 'ec2-54-162-119-125.compute-1.amazonaws.com',
-    user : 'hmoqwspeinytnc',
-    password : 'dfe401ba6d7e4553d06c4a37e7bc69eae6d7a0e8114bdc6a81e87c9ad2d639e2',
-    database : 'd5o9pd7sf63vsd'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
   }
 });
 
+db.select('*').from('users');
+
+
 const app = express();
-
-app.use(cors())
 app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log('app is running on port ${process.env.PORT}');
-})
+
+/*-----------------------         ROUTES       -------------------*/
+
+app.get('/', (req, res) => {res.send(database.users);});
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, bcrypt, db)});
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, bcrypt, db)});
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
+
+app.post('/imageurl', (req, res) => {image.handleAPICall(req, res)});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app running in port ${process.env.PORT}`);
+});
