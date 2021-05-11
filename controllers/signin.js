d77804d38b03a6cc58f13bbeaf7a5b4d11e5349e
Shreya Gupta <55207479+gshreya10@users.noreply.github.com>
2021-05-11 18:30:30 +0530

    Update signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,5 +1,4 @@
-  
-const handleSignin = (req, res, db, bcrypt) => {
+  const handleSignin = (req, res, db, bcrypt) => {
     const { email, password } = req.body;
     if (!email || !password) {
       return res.status(400).json('incorrect form submission');
